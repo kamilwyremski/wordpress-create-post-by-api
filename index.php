b32c8efeb59845dc98b70a1e502b8a8958b95381
Kamil Wyremski <kamil.wyremski@gmail.com>
2024-01-09 12:53:40 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,107 @@
+<?php
+
+$envFilePath = __DIR__ . '/.env';
+
+if (!file_exists($envFilePath)) {
+  die('.env file not found.');
+}
+
+$envContent = file_get_contents($envFilePath);
+$lines = explode("\n", $envContent);
+
+foreach ($lines as $line) {
+  $line = trim($line);
+
+  if (!empty($line) && strpos($line, '=') !== false) {
+    list($key, $value) = explode('=', $line, 2);
+    $key = trim($key);
+    $value = trim($value);
+    putenv("$key=$value");
+    $_ENV[$key] = $value;
+  }
+}
+
+require_once('wordpress-api.class.php');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Add post to Wordpress</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+  <div class="container mt-5">
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST" and !empty($_POST['title']) and !empty($_POST['lid']) and !empty($_POST['content'])) {
+
+      $wordpressApi = new WordpressApi(getenv('WORDPRESS_DOMAIN'), getenv('WORDPRESS_USERNAME'), getenv('WORDPRESS_PASSWORD'));
+
+      $thumbnail_id = null;
+      if (!empty($_POST['thumb'])) {
+        try {
+          $thumbnail_id = $wordpressApi->addMedia($_POST['thumb'], $_POST['title'])['id'];
+        } catch (\Exception $e) {
+          echo ('<p>Error sending image: ' . $e->getMessage() . '</p>');
+        }
+      }
+      $tags = [];
+      if ($_POST['tags']) {
+        $new_tags = explode(',', $_POST['tags']);
+        foreach ($new_tags as $tag) {
+          try {
+            $tags[] = $wordpressApi->addTag($tag)['id'];
+            echo ('<p>Tag created successfully: ' . $new_tag_name . '</p>');
+          } catch (\Exception $e) {
+            echo ('<p>Error creating tag: ' . $e->getMessage() . '</p>');
+          }
+        }
+      }
+
+      try {
+        $post = $wordpressApi->addArticle($_POST['title'], $_POST['content'], $_POST['lid'], $tags, $thumbnail_id);
+        echo('<p>You have successfully added an article. Post ID: ' . $post['id'] . '</p>');
+      } catch (\Exception $e) {
+        echo('<p>Error: ' . $e->getMessage() . '</p>');
+      }
+    }
+    ?>
+  
+    <h1 class="mb-4">Add post to Wordpress</h1>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+      <div class="mb-3">
+        <label for="title" class="form-label">Title:</label>
+        <input type="text" name="title" required class="form-control" id="title">
+      </div>
+
+      <div class="mb-3">
+        <label for="lid" class="form-label">LID:</label>
+        <textarea name="lid" rows="2" required class="form-control" id="lid"></textarea>
+      </div>
+
+      <div class="mb-3">
+        <label for="content" class="form-label">Content:</label>
+        <textarea name="content" rows="4" required class="form-control" id="content"></textarea>
+      </div>
+
+      <div class="mb-3">
+        <label for="tags" class="form-label">Tags (enter after comma):</label>
+        <input type="text" name="tags" class="form-control" id="tags">
+      </div>
+
+      <div class="mb-3">
+        <label for="thumb" class="form-label">Link to image:</label>
+        <input type="url" name="thumb" class="form-control" id="thumb">
+      </div>
+
+      <button type="submit" class="btn btn-primary">Send</button>
+    </form>
+  </div>
+
+</body>
+
+</html>
---
+++ wordpress-api.class.php
@@ -0,0 +1,123 @@
+<?php
+
+class WordpressApi
+{
+  private $domain;
+  private $username;
+  private $password;
+
+  public function __construct(string $domain, string $username, string $password)
+  {
+    $this->domain = $domain;
+    $this->username = $username;
+    $this->password = $password;
+  }
+
+  function addMedia(string $url, string $filename = ''){
+    $image_data = file_get_contents($url);
+    if ($image_data === false) {
+      throw new \Exception('Error fetching image from URL.');
+    }
+    $boundary = uniqid();
+    if(!$filename){
+      $filename = pathinfo(basename($url), PATHINFO_FILENAME);
+    }
+    $filename = mb_strtolower($filename);
+    $data = "--$boundary\r\n" .
+      "Content-Disposition: form-data; name=\"file\"; filename=\"".$filename.".jpg\"\r\n" .
+      "Content-Type: image/jpeg\r\n\r\n" .
+      "$image_data\r\n" .
+      "--$boundary--\r\n";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $this->domain . '/wp-json/wp/v2/media');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+      'Authorization: Basic '. base64_encode($this->username . ':' . $this->password),
+      'Content-Type: multipart/form-data; boundary=' . $boundary,
+      'Content-Length: ' . strlen($data),
+    ]);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    $response = curl_exec($ch);
+    if ($response === false) {
+      throw new \Exception(curl_error($ch));
+    }
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    if ($http_code == 201) {
+      return json_decode($response ,true);
+    } else {
+      throw new \Exception($http_code);
+    }
+  }
+
+  function addTag($tag){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $this->domain . '/wp-json/wp/v2/tags');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+      'Authorization: Basic '. base64_encode($this->username . ':' . $this->password),
+      'Content-Type: application/json; charset=utf-8',
+    ]);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
+      'name' => trim($tag)
+    ]));
+    $response = curl_exec($ch);
+    curl_close($ch);
+    if ($response) {
+      $new_tag = json_decode($response, true);
+      if(!empty($new_tag['id'])){
+        return $new_tag;
+      }else{
+        throw new \Exception('Error creating tag');
+      }
+    } else {
+      throw new \Exception('Error creating tag');
+    }
+  }
+
+  private function replaceImageLinks($matches) {
+    $imageData = $this->addMedia($matches[1]);
+    return str_replace($matches[1], $imageData['guid']['rendered'], $matches[0]);
+}
+
+  function addArticle(string $title, string $body = '', string $lid = '', array $tags = [], string $thumbnail_id = null){
+
+    $pattern = '/<img[^>]+src=[\'"]([^\'"]+)[\'"][^>]*>/';
+    $body = preg_replace_callback($pattern, [$this, 'replaceImageLinks'], $body);
+
+    $postfields = json_encode([
+      'status' => 'publish',
+      'tags' => $tags,
+      'title' => $title,
+      'content' => $body,
+      'excerpt' => $lid,
+      'featured_media' => $thumbnail_id
+      ]);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $this->domain . '/wp-json/wp/v2/posts');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+      'Authorization: Basic '. base64_encode($this->username . ':' . $this->password),
+      'Content-Type: application/json; charset=utf-8',
+    ]);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    if ($response) {
+      $post = json_decode($response, true);
+      if (!empty($post['id'])) {
+        return $post;
+      } else {
+        throw new \Exception($post['message']);
+      }
+    }else{
+      throw new \Exception('Error communicating with WordPress API.');
+    }
+  }
+}
